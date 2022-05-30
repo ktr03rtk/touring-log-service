b7e9d79c82503789a0b0575fedb1520edc227655
ktr03 <ktrsouth@gmail.com>
2022-05-31 01:20:52 +0900

    feature: payload subscribe usecase

---
+++ data-store/usecase/payload_subscribe_usecase.go
@@ -0,0 +1,35 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/ktr03rtk/touring-log-service/data-store/domain/model"
+	"github.com/ktr03rtk/touring-log-service/data-store/domain/repository"
+	"github.com/pkg/errors"
+)
+
+const (
+	concurrency = 100
+)
+
+type PayloadSubscribeUsecase interface {
+	Execute(context.Context, <-chan *model.Payload) error
+}
+
+type payloadSubscribeUsecase struct {
+	payloadSubscribeRepository repository.PayloadSubscribeRepository
+}
+
+func NewPayloadSubscribeUsecase(pr repository.PayloadSubscribeRepository) PayloadSubscribeUsecase {
+	return &payloadSubscribeUsecase{
+		payloadSubscribeRepository: pr,
+	}
+}
+
+func (pu *payloadSubscribeUsecase) Execute(ctx context.Context, ch <-chan *model.Payload) error {
+	if err := pu.payloadSubscribeRepository.Subscribe(ctx, ch); err != nil {
+		return errors.Wrapf(err, "failed to execute payload subscribe usecase")
+	}
+
+	return nil
+}
