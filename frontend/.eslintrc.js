62ec24e1a77df8e6c17122711edcc08cd9b40c65
ktr03 <ktrsouth@gmail.com>
2022-06-27 01:21:25 +0900

    add eslint and prettier config

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'import', 'unused-imports'],
+  rules: {
+    'unused-imports/no-unused-imports': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'object', 'type', 'index'],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroups: [
+          {
+            pattern: '@/components/common',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/hooks',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+      },
+    ],
+  },
+};
