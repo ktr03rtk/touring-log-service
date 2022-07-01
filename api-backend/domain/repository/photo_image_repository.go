100498aad64d8c5763e4c88c49631a4e623fa882
ktr03 <ktrsouth@gmail.com>
2022-07-01 19:29:28 +0900

    repository: photo image

---
+++ api-backend/domain/repository/photo_image_repository.go
@@ -0,0 +1,10 @@
+//go:generate mockgen -source=photo_image_repository.go -destination=../../mock/mock_photo_image_repository.go -package=mock
+package repository
+
+import (
+	"io"
+)
+
+type PhotoImageRepository interface {
+	Store(io.Reader, string) error
+}
--- api-backend/infrastructure/persistence/photo_metadata_persistence.go
+++ api-backend/infrastructure/persistence/photo_metadata_persistence.go
@@ -18,11 +18,33 @@ func NewPhotoMetadataPersistence(conn *gorm.DB) repository.PhotoMetadataReposito
 	}
 }
 
-func (tp *PhotoMetadataPersistence) Create(photo *model.Photo) error {
-	if err := tp.conn.Create(&photo).Error; err != nil {
+func (mp *PhotoMetadataPersistence) Create(photo *model.Photo) error {
+	if err := mp.conn.Create(&photo).Error; err != nil {
 		return errors.Wrapf(err, "failed to create photo. photo: %+v", &photo)
 	}
 
 	return nil
 }
 
+// func (mp *PhotoMetadataPersistence) FindByID(id model.PhotoID) (*model.Photo, error) {
+// 	t := &model.Photo{ID: id}
+
+// 	if err := mp.conn.First(&t).Error; err != nil {
+// 		return nil, errors.Wrapf(err, "failed to find photo. id: %+v", id)
+// 	}
+
+// 	return t, nil
+// }
+
+// func (mp *PhotoMetadataPersistence) FindAll() ([]*model.Photo, error) {
+// 	var tasks []*model.Photo
+// 	if err := mp.conn.Find(&tasks).Error; err != nil {
+// 		return nil, errors.Wrapf(err, "failed to find all tasks")
+// 	}
+
+// 	return tasks, nil
+// }
+
+// func (mp *PhotoMetadataPersistence) Update(t *model.Photo) error {
+// 	return mp.conn.Save(&t).Error
+// }
