5daafa19d44450c02d298fb315fa29e21a47bc35
ktr03 <52913281+ktr03rtk@users.noreply.github.com>
2022-11-06 20:19:41 +0900

    fix navbar toggle

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -53,14 +53,14 @@ const Nav = ({ jwt, setJwt }: NavProperties) => {
           className='navbar-toggler'
           type='button'
           data-bs-toggle='collapse'
-          data-bs-target='#navbarTogglerDemo02'
-          aria-controls='navbarTogglerDemo02'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
           aria-expanded='false'
           aria-label='Toggle navigation'
         >
           <span className='navbar-toggler-icon'></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <div className='collapse navbar-collapse' id='navbarNav'>
           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
             <li className='nav-item'>
               <Link to='/' className='nav-link'>
